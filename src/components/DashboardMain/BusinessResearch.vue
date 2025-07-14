1e22e9307b9f9e8e0e30dd4933f098f9fb87fdef
Yana Barak <yanabarak94@gmail.com>
2025-07-14 15:18:30 +0300

    changes

--- src/components/DashboardMain/BusinessResearch.vue
+++ src/components/DashboardMain/BusinessResearch.vue
@@ -157,6 +157,43 @@
           </ul>
         </div>
       </div>
+
+
+      <div class="block-type col-12">
+        <div class="d-flex gap-2 align-items-center sec-color fs-16">
+          <div class="icons-wrapper">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-buildings"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022M6 8.694 1 10.36V15h5zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5z"
+              />
+              <path
+                d="M2 11h1v1H2zm2 0h1v1H4zm-2 2h1v1H2zm2 0h1v1H4zm4-4h1v1H8zm2 0h1v1h-1zm-2 2h1v1H8zm2 0h1v1h-1zm2-2h1v1h-1zm0 2h1v1h-1zM8 7h1v1H8zm2 0h1v1h-1zm2 0h1v1h-1zM8 5h1v1H8zm2 0h1v1h-1zm2 0h1v1h-1zm0-2h1v1h-1z"
+              />
+            </svg>
+          </div>
+          Product Pipeline
+        </div>
+        <div>
+          <ul class="neutral-color pt-0">
+            <li class="pl-2 d-block d-md-flex justify-content-between">
+              <router-link to="Ideation">Ideation</router-link>
+            </li>
+            <li class="pl-2 d-block d-md-flex justify-content-between">
+              <router-link to="In_Development">In Development</router-link>
+            </li>
+            <li class="pl-2 d-block d-md-flex justify-content-between">
+              <router-link to="Archived">Archived</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
